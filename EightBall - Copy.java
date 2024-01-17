2007737bbaefa0f58e0a3347042f2871b3421d3b
wydododjs <wydododjaswin@gmail.com>
2024-01-17 14:09:32 +0700

    add source code

--- EightBall - Copy.java
+++ EightBall - Copy.java
@@ -67,3 +67,16 @@ public class EightBall {
 		System.out.println(buffer);
 	}
 }
+}public class EightBall {
+	public static void main(String args[]) throws Exception {
+		char[] buffer = new char[1024];
+		String filename = args[0];
+		try {
+			filename = "" + (Integer.parseInt(filename) % 3);
+		} catch (Exception e) {
+			System.out.println("Invalid input.");
+		}
+		new FileReader(filename).read(buffer);
+		System.out.println(buffer);
+	}
+}
